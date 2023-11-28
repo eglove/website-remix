191f93be9ada45b09eb2b447bc4cba8d2f4b1764
Ethan Glover <hello@ethang.email>
2023-11-28 16:32:05 -0600

    Add color conversion functionality in the app.
    This commit adds a color conversion feature to the app. New dependencies, `color-convert` and `zod`, were added to help facilitate the conversion of color values from Hex format to RGB, HSL, HWB, and Keyword format. Furthermore, the types package for `color-convert` is added to the devDependencies for better type checking and support. This color conversion feature is implemented in a new file, `_blog.blog.hex-converter.tsx`. This addition will provide users with a more comprehensive and practical tool for color manipulation in the app. The code was designed to ensure efficiency and reliability while providing a user-friendly interface for color conversion.

---
+++ app/routes/_blog.blog.hex-converter.tsx
@@ -0,0 +1,108 @@
+import { useForm } from '@ethang/use-form';
+import { Card, CardBody, CardHeader } from '@nextui-org/card';
+import { Input } from '@nextui-org/input';
+import { Snippet } from '@nextui-org/snippet';
+import convert from 'color-convert';
+import type { JSX } from 'react';
+import { z } from 'zod';
+
+import styles from '../assets/css/snippet.module.css';
+import { Paragraph } from '../components/elements/paragraph';
+
+const hexConverterSchema = z.object({
+  color: z
+    .string()
+    .regex(/^[\dA-Fa-f]{3,6}$/, 'Invalid Hex Format')
+    .transform(color => {
+      if (!color.startsWith('#')) {
+        return `#${color}`;
+      }
+
+      return color;
+    })
+    .default('#000'),
+});
+
+export default function (): JSX.Element {
+  const { handleChange, formState, handleSubmit, fieldErrors } = useForm(
+    { color: '#000' },
+    {
+      zodValidator: hexConverterSchema,
+    },
+  );
+
+  const hsl = convert.hex.hsl(formState.color);
+  const hslString = `hsl(${hsl[0]}deg ${hsl[1]}% ${hsl[2]}%)`;
+
+  const rgb = convert.hex.rgb(formState.color);
+  const rgbString = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+
+  const hwb = convert.hex.hwb(formState.color);
+  const hwbString = `hwb(${hwb[0]}deg ${hwb[1]}% ${hwb[2]}%)`;
+
+  const keyword = convert.hex.keyword(formState.color);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <fieldset className="mx-auto my-4 max-w-fit">
+          {typeof fieldErrors?.color === 'string' && (
+            <Paragraph className="text-red-500">
+              {fieldErrors?.color ?? ''}
+            </Paragraph>
+          )}
+          <Input
+            label="Hex Color"
+            name="color"
+            value={formState.color}
+            variant="bordered"
+            onChange={handleChange}
+          />
+        </fieldset>
+      </form>
+
+      <div className="flex flex-wrap gap-2">
+        <Card className="grow border-2" shadow="none">
+          <CardHeader className="bg-black text-white">Keyword</CardHeader>
+          <CardBody style={{ backgroundColor: keyword }}>
+            <Snippet
+              className={styles.Snippet}
+              color="primary"
+              variant="solid"
+            >{`color: ${keyword};`}</Snippet>
+          </CardBody>
+        </Card>
+        <Card className="grow border-2" shadow="none">
+          <CardHeader className="bg-black text-white">HSL</CardHeader>
+          <CardBody style={{ backgroundColor: hslString }}>
+            <Snippet
+              className={styles.Snippet}
+              color="primary"
+              variant="solid"
+            >{`color: ${hslString};`}</Snippet>
+          </CardBody>
+        </Card>
+        <Card className="grow border-2" shadow="none">
+          <CardHeader className="bg-black text-white">RGB</CardHeader>
+          <CardBody style={{ backgroundColor: rgbString }}>
+            <Snippet
+              className={styles.Snippet}
+              color="primary"
+              variant="solid"
+            >{`color: ${rgbString};`}</Snippet>
+          </CardBody>
+        </Card>
+        <Card className="grow border-2" shadow="none">
+          <CardHeader className="bg-black text-white">HWB</CardHeader>
+          <CardBody style={{ backgroundColor: hwbString }}>
+            <Snippet
+              className={styles.Snippet}
+              color="primary"
+              variant="solid"
+            >{`color: ${hwbString};`}</Snippet>
+          </CardBody>
+        </Card>
+      </div>
+    </>
+  );
+}
