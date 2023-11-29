88ec4e08c281c63118bc056f61534b750c685cfd
Ethan Glover <hello@ethang.email>
2023-11-29 17:05:59 -0600

    add blog post

---
+++ app/routes/_blog.blog.avoiding-third-party-libraries.tsx
@@ -0,0 +1,55 @@
+import { A } from '../components/elements/a';
+import { Paragraph } from '../components/elements/paragraph';
+
+export default function () {
+  return (
+    <>
+      <Paragraph>
+        As a developer, it can be tempting to reach for the latest and greatest
+        libraries and frameworks to solve every problem. However, it&apos;s
+        important to remember that sometimes the simplest solution is the best
+        one.
+      </Paragraph>
+      <Paragraph>
+        In the world of web development, there are many libraries and frameworks
+        that offer solutions for common problems, such as authentication and
+        managing application state. However, these libraries often come with
+        their own set of problems, such as bloat and potential security
+        vulnerabilities.
+      </Paragraph>
+      <Paragraph>
+        For example, when it comes to authentication, many developers use
+        specialized libraries like{' '}
+        <A href="https://next-auth.js.org">NextAuth</A> to handle the process.
+        However, HTTP headers and cookies already exist for this purpose and are
+        natively supported by web browsers. Using these built-in tools can often
+        be a more secure and efficient solution than relying on a third-party
+        library.
+      </Paragraph>
+      <Paragraph>
+        Or the use of global providers for managing light theme vs dark theme.
+        While these libraries can be useful, the
+        &ldquo;prefers-color-scheme&rdquo; media query can be used to access the
+        user&apos;s device settings and provide a more consistent experience
+        across all of their apps.
+      </Paragraph>
+      <Paragraph>
+        Furthermore, when it comes to network data, many developers will try to
+        shove everything into a global provider such as with Redux. But state
+        management tools don&apos;t properly use HTTP cache invalidation and
+        overcomplicate the idea of caching at all. In these cases, it&apos;s
+        often better to use a simpler approach that does support caching such as{' '}
+        <A href="https://tanstack.com/query/v4/">React Query</A>,{' '}
+        <A href="https://swc.rs">SWC</A>, or the upcoming features built into{' '}
+        <A href="https://beta.nextjs.org/docs/data-fetching/caching">NextJS</A>.
+      </Paragraph>
+      <Paragraph>
+        While the React ecosystem offers many powerful tools, it&apos;s
+        important for developers to remember the native solutions that are
+        already available. By using these simple, built-in tools, we can avoid
+        the pitfalls of relying on third-party libraries and create more
+        efficient and secure applications.
+      </Paragraph>
+    </>
+  );
+}
---
+++ app/routes/_blog.blog.stackoverflow-not-help-forum.tsx
@@ -0,0 +1,106 @@
+import { A } from '../components/elements/a';
+import { Paragraph } from '../components/elements/paragraph';
+
+export default function () {
+  return (
+    <>
+      <Paragraph>
+        StackOverflow has for a very long time been described as{' '}
+        <A href="https://www.reddit.com/r/webdev/comments/7szv7i/anyone_else_find_the_stack_overflow_community/">
+          toxic
+        </A>{' '}
+        and{' '}
+        <A href="https://meta.stackexchange.com/questions/342779/what-about-the-community-is-toxic-to-new-users">
+          unwelcoming
+        </A>{' '}
+        to beginners. Or really unwelcoming to anyone that is not very deep into
+        StackOverflow&apos;s very specific niche rules and mentality.
+      </Paragraph>
+      <Paragraph>
+        The chances of being able to ask a simple question and get genuine help
+        or an answer are very low. You can spend multiple days working to ask
+        the perfect question. You can follow every rule laid out in this{' '}
+        <A href="https://stackoverflow.com/help/how-to-ask">
+          1,281 word article
+        </A>{' '}
+        and{' '}
+        <A href="https://codeblog.jonskeet.uk/2010/08/29/writing-the-perfect-question/">
+          this referenced 3,141 word article
+        </A>
+        . But your question is still most likely to be downvoted, locked, and
+        peppered with petty complaints by &ldquo;mods&rdquo; and &ldquo;super
+        users.&rdquo;
+      </Paragraph>
+      <Paragraph>
+        What StackOverflow is trying to do, in theory, makes sense. As the{' '}
+        <A href="https://stackoverflow.com">homepage says</A>, StackOverflow is,
+        &ldquo;A public platform building the definitive collection of coding
+        questions & answers.&rdquo;
+      </Paragraph>
+      <Paragraph>
+        Meaning, StackOverflow is not a QA forum. It is meant to be a place
+        where specific questions have definitive answers. As with Wikipedia,
+        duplicate pages are not allowed, spelling errors are regularly fixed,
+        and anything that doesn&apos;t follow a specific format and standard is
+        removed.
+      </Paragraph>
+      <Paragraph>
+        The idea that there can be an encyclopedia for very specific questions
+        about code is, to me, a fundamentally flawed one. Code evolves too
+        often. There&apos;s an infinite number of problems and an infinite
+        number of solutions. The idea just doesn&apos;t make sense. Beginners
+        need to be able to ask &ldquo;stupid questions&rdquo; until those
+        neurons start to connect in a way that they can start creating smart
+        solutions.
+      </Paragraph>
+      <Paragraph>
+        But for the top percentage of SO users, this idea of SO being an
+        encyclopedia instead of a forum must be protected at all costs. This is
+        what makes it feel so toxic and unwelcoming. You can&apos;t just ask a
+        question and get an answer. You instead must contribute to this
+        encyclopedia of definitive questions and definitive answers.
+      </Paragraph>
+      <Paragraph>
+        Today&apos;s developers are better off with using tools like CoPilot,
+        CodeWhisperer, TabNine, or Kite to get inline suggestions for languages
+        they are unfamiliar with. These provide better feedback and cost less
+        time than trying to force an SO answer on your particular project or
+        spending days trying to formulate the perfect encyclopedic question.
+      </Paragraph>
+      <Paragraph>
+        This may be my personal Google history, but overtime I have started to
+        see more results for GitHub issues than StackOverflow when looking for
+        different problems.
+      </Paragraph>
+      <Paragraph>
+        I think package and library maintainers of OSS on GitHub are far more
+        welcoming and of course far more helpful than their SO counterparts.
+        However, this starts to create a lot of noise. For maintainers, issues
+        are meant to be for legitimate bug reports and feature requests. Not for
+        issues beginners have with using their library. So when asking
+        questions, people are often redirected to a Discord channel or help
+        forum specific to the team that&apos;s maintaining the repo.
+      </Paragraph>
+      <Paragraph>
+        This is great, but there&apos;s still a problem. Having to hunt down the
+        appropriate place to ask questions for every tool. It&apos;s not always
+        obvious where you&apos;re supposed to go to get a simple answer to a
+        simple question.
+      </Paragraph>
+      <Paragraph>
+        I feel like GitHub could solve this problem well. By creating a general
+        forum that can reference specific repositories. This allows package
+        maintainers to watch questions about their work without burying
+        important issues. And it allows people to help one another without
+        following specific repo conversations.
+      </Paragraph>
+      <Paragraph>
+        The world of getting help with code online is a bit fractured, it&apos;s
+        hard to know where to go. StackOverflow is no longer a sensible option.
+        Reddit is... it&apos;s Reddit, always has been. It would be nice to see
+        a very popular platform like GitHub take on the challenge of creating a
+        safe space for developers to get help and help one another.
+      </Paragraph>
+    </>
+  );
+}
