8a2208370f62475fdb576b68494a40c54c0e9b5f
Ethan Glover <hello@ethang.email>
2023-11-29 17:14:53 -0600

    add blog post

---
+++ app/routes/_blog.blog.dont-adopt-new-technologies-if-youre-not-ready.tsx
@@ -0,0 +1,104 @@
+import { A } from '../components/elements/a';
+import { Paragraph } from '../components/elements/paragraph';
+
+export default function () {
+  return (
+    <>
+      <Paragraph>
+        The release of
+        <A href="https://nextjs.org/blog/next-13">NextJS 13</A> changes how we
+        build React apps. Server components as a default, first-class Promise
+        support, and caching built directly into the router. And I couldn&apos;t
+        be more excited. I&apos;ve converted this blog to use the new app
+        directory (with many headaches and apparently some sacrifices to
+        responsiveness). This is expected, it is a beta feature after all.
+      </Paragraph>
+      <Paragraph>
+        But I wanted to take a step back and remind people that if they&apos;re
+        angry about the direction Next is going in, or fail to see why it&apos;s
+        better than [insert other thing here]. Then you not only don&apos;t have
+        to use it, I would plead that you don&apos;t try.
+      </Paragraph>
+      <Paragraph>
+        I&apos;ve switched technologies a lot. For the longest time I hated
+        JavaScript. I was all about PHP and going out of my way to never write a
+        single line of JS. I even used{' '}
+        <A href="https://amp.dev/documentation/guides-and-tutorials/learn/spec/amphtml/">
+          Google AMP HTML
+        </A>{' '}
+        very heavily when it released. Before PHP, I wanted to do everything in
+        Java. But when I was introduced to React, I loved it.
+      </Paragraph>
+      <Paragraph>
+        It&apos;s the time that I hated JS that I want to focus on. Everyone has
+        their biases. I&apos;ve seen hate of unfamiliar things in teams as well.
+        I was once hired to help a team move from Apache Velocity to Vue. They
+        wanted Vue because it <span className="font-bold">looked</span>{' '}
+        familiar. But they failed to grasp the fundamentals of client-side
+        routing, client-side rendering, component based design, state
+        management, and so on. I saw the same thing with a PHP team later down
+        the line.
+      </Paragraph>
+      <Paragraph>
+        The reason they failed to get it is because they wanted to keep using
+        what they were familiar with. They wanted to force Vue to work inside of
+        their current mental models.{' '}
+        <span className="font-bold">
+          They romanticized the idea of being &ldquo;modern&rdquo;, but refused
+          to learn what that means.
+        </span>
+      </Paragraph>
+      <Paragraph>
+        I recommended that they keep using Apache Velocity and do not attempt to
+        switch stacks until they&apos;re ready to A) Learn outside of work, not
+        just think they can hack it together within 8 hours a day on user
+        stories, and B) take it seriously.
+      </Paragraph>
+      <Paragraph>
+        If you&apos;re not motivated by improvements or new things frustrate
+        you, there&apos;s nothing wrong with that. You can rant all day about
+        whatever you like, I like to rant too. But please, do not try to force
+        your biases on those of us who are always looking to learn and improve.
+      </Paragraph>
+      <Paragraph>
+        Diversity in thinking is great, but by swimming against the tide, you
+        become the chink in the armor in any team you work with.
+      </Paragraph>
+      <Paragraph>
+        If you prefer an &ldquo;older&rdquo; way of doing things, I hold no
+        stigma against that. Just do it. Find a company that agrees with you.
+        You can deliver good user experiences either way. Just don&apos;t try to
+        jump in anyone&apos;s path and try to lead them off of it in reaction to
+        your own personal fears. You can&apos;t stop the world from moving
+        forward. The movements of technology are bigger than you, I, any dumb
+        little project we work on, and even Vercel (as difficult to see as that
+        is right now).
+      </Paragraph>
+      <Paragraph>
+        If you&apos;re going to hop on this NextJS train, you need to be on
+        board at least to an extent that you&apos;re not scoffing and rolling
+        your eyes like a child. Believe me, if there were something better
+        I&apos;d switch technologies again, just as I&apos;ve always done.
+      </Paragraph>
+      <Paragraph>
+        The same goes for all technologies, if you&apos;re going to switch from
+        Apache Velocity to Vue, you had better be ready to forget everything you
+        know about building frontends. The alternative is to stick with Apache
+        Velocity, there&apos;s nothing wrong with that.
+      </Paragraph>
+      <Paragraph>
+        If you&apos;re going to hop on the Node train, insisting on a DI
+        framework like NestJS is just an unwillingness to learn Node and the JS
+        ecosystem. The alternative is to stick with Spring Boot, there&apos;s
+        nothing wrong with that.
+      </Paragraph>
+      <Paragraph>
+        By trying to force the wrong biases into a community that has rejected
+        them, you&apos;re just creating tension, you are the elephant in the
+        room that doesn&apos;t belong. Let&apos;s accept that differences in
+        doing things is normal and OK, and that not everyone has to be just like
+        us.
+      </Paragraph>
+    </>
+  );
+}
---
+++ app/routes/_blog.blog.react-development-about-change.tsx
@@ -0,0 +1,159 @@
+import { A } from '../components/elements/a';
+import { Blockquote } from '../components/elements/blockquote';
+import { Heading } from '../components/elements/heading';
+import { Paragraph } from '../components/elements/paragraph';
+import { SanityImage } from '../components/elements/sanity-image';
+
+export default function () {
+  return (
+    <>
+      <Paragraph>
+        Next 12 introduced an alpha version of server components but it seems
+        not a lot of people bothered to try them out. But with the introduction
+        of nested layouts and server components as a default in beta with Next
+        13, it seems everyone is jumping to try to get it all implemented and
+        working as soon as possible.
+      </Paragraph>
+      <Paragraph>
+        I will say that layouts are rough. Unless you&apos;re doing everything
+        static, there are a lot of issues. A lot of components that use React
+        hooks just don&apos;t work with &ldquo;client components&rdquo;. Third
+        party libraries aren&apos;t likely to work with Next 13 layouts just
+        yet.
+      </Paragraph>
+      <Paragraph>
+        That being said, this is all bringing huge changes to the way we build
+        React apps. Whether you use NextJS or not.
+      </Paragraph>
+      <Heading variant="h3">React is Officially Recommending NextJS</Heading>
+      <Blockquote
+        link="https://react.dev/learn/start-a-new-react-project"
+        source="Start a New React Project"
+      >
+        If you&apos;re learning React, we recommend Create React App. ... If
+        you&apos;re looking to start a production-ready project, Next.js is a
+        great place to start.
+      </Blockquote>
+      <Paragraph>
+        To understand why React is moving in this direction of recommending
+        frameworks, it&apos;s worth noting what they said at Next.js Conf 2022.
+      </Paragraph>
+      <Blockquote source="Next.js Conf 2022">
+        I&apos;ve been thinking of NextJS 13 as really like, this is the real
+        React 18 release. Because even though we have a lot of primitives in
+        React 18, you can&apos;t really take advantage of them unless you have a
+        framework that takes... that is built on top of those architectural
+        primitives. So on Facebook.com, Facebook has an infrastructure
+        that&apos;s based on Relay and some other routing concepts that... I
+        mean one way to think of it is that NextJS is an open source
+        implementation of that. In some ways it even goes a step further now
+        with server components. I&apos;m really excited for the rest of the
+        React community to now be able to take advantage of all of these
+        improvements that we spent so many years researching and building.
+      </Blockquote>
+      <Paragraph>
+        Or to solidify this further, it&apos;s worth reading Dan Abramov&apos;s
+        thoughts on Next 13.
+      </Paragraph>
+      <Blockquote
+        link="https://twitter.com/dan_abramov/status/1585076899126345728"
+        source="Dan Abramov"
+      >
+        on the other hand, for the past five years, much of our work has been
+        directed towards features that can only truly shine within a deeper
+        stack integration. the most basic one is server rendering; it makes
+        sense to render React to HTML, but this requires a bit of manual wiring!
+      </Blockquote>
+      <Blockquote
+        link="https://twitter.com/dan_abramov/status/1585076899126345728"
+        source="Dan Abramov"
+      >
+        this is why I like to think of React as two things. React is a library.
+        it is *also* an architecture (which frameworks may implement). this
+        architecture doesn&apos;t specify things like filename conventions, but
+        it&apos;s focused on pieces where deep integration with React has most
+        value.
+      </Blockquote>
+      <Heading variant="h3">Caching = Global State</Heading>
+      <Paragraph>
+        You may have already ditched global state providers like Redux for
+        client-side cache invalidation via React Query or SWR, and switched to{' '}
+        <A href="https://beta.reactjs.org/learn/managing-state">
+          Reacts recommended way of handling state management
+        </A>
+        . (
+        <A href="https://hswolff.com/blog/why-i-love-usereducer/">
+          Harry Wolff recommended this pattern
+        </A>{' '}
+        2 years before it became a part of official docs.) If you have,
+        you&apos;re already familiar with this concept, if you haven&apos;t,
+        it&apos;s coming.
+      </Paragraph>
+      <Paragraph>
+        Not only does the new router in Next 13 introduce global caching and
+        deduplication,{' '}
+        <A href="https://github.com/facebook/react/blob/main/packages/react/src/ReactFetch.js#L42">
+          but React is now overriding the global fetch method to add caching
+        </A>
+        . Of course this is a very controversial decision as overriding native
+        methods isn&apos;t exactly in good taste.
+      </Paragraph>
+      <SanityImage
+        alt="React overrides globalThis.fetch instead of using import {fetch}"
+        height={Math.round(1117 / 3)}
+        src="https://cdn.sanity.io/images/drccvtog/production/612503262c745fc1a51a67a922ec84f77e0e57ac-1095x1117.jpg"
+        width={Math.round(1095 / 3)}
+      />
+      <Paragraph>
+        But overall the point is, with data fetching handled by built in caching
+        in both React and NextJS, the need for state management decreases
+        significantly. Suddenly the idea of only creating small, scoped
+        contexts, makes a lot more sense than a global provider.
+      </Paragraph>
+      <Paragraph>
+        It&apos;s important to remember that NextJS components in the beta app/
+        directory are server side by default. And on the server, context
+        doesn&apos;t exist. I would of course say to avoid the temptation to
+        just make everything client side just to force old mental models.
+      </Paragraph>
+      <Paragraph>
+        I have enough experience building highly dynamic complex apps without
+        global providers to give the &ldquo;but my app is bigger than your app,
+        you just don&apos;t understand&rdquo; much attention.
+      </Paragraph>
+      <Heading variant="h3">Edge Deployments</Heading>
+      <Paragraph>
+        Serverless deployments are coming in hot whether we want to embrace it
+        or not. There is still a minority of projects out there trying to
+        deliver React apps as a single bundle on S3 buckets or CDN.
+      </Paragraph>
+      <Paragraph>
+        This is not realistic for the modern dynamic app. As apps have grown, we
+        know the performance hit they take on hydration. Trying to load all JS
+        at once just doesn&apos;t make sense. Nor does it make sense to make
+        everything static again.
+      </Paragraph>
+      <Paragraph>
+        By delivering frontend servers to the edge that can build, render, and
+        create a client side cache for each user, we can have customizable,
+        dynamic experiences without the sacrifice to performance.
+      </Paragraph>
+      <Heading variant="h3">It&apos;s Not Just NextJS</Heading>
+      <Paragraph>
+        It&apos;s not just that React has cozied up close with Vercel. Or
+        it&apos;s not that Vercel has just influenced React. Shopify Hydrogen
+        and Gatsby have been very influential in making these changes too.
+        Because they&apos;re all trying to solve the same problems.
+      </Paragraph>
+      <Paragraph>
+        This has been a collaborative effort and many years of work to make
+        happen. If you haven&apos;t already, it&apos;s worth going in to the{' '}
+        <A href="https://react.dev">new React documentation</A> and{' '}
+        <A href="https://beta.nextjs.org/docs">NextJS Beta documentation</A>{' '}
+        with a clear mind and intention to relearn everything you know about
+        working with React. Because I believe this is the direction the rest of
+        the world is going to go in with or without us.
+      </Paragraph>
+    </>
+  );
+}
