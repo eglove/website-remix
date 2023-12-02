eef67fef911d20da93073ef6b6738789c7e9f6f2
Ethan Glover <hello@ethang.email>
2023-12-02 13:31:12 -0600

    Add two new blog posts in the portfolio
    Two new blog posts have been added named 'A First Post' and 'PC backup in Two Lines'. 'A First Post' discusses the evolution and choices behind the new portfolio while 'PC backup in Two Lines' provides insights into the process of setting up a PC backup. Both articles are aimed at sharing development experiences and knowledge with the audience.

---
+++ app/routes/_blog.blog.a-first-post.tsx
@@ -0,0 +1,75 @@
+import { A } from '../components/elements/a';
+import { Paragraph } from '../components/elements/paragraph';
+
+export default function () {
+  return (
+    <>
+      <Paragraph>
+        It&apos;s been a while since I&apos;ve had a blog. My first was on
+        BlogSpot. It contained a story called, ‘The Incredibly Close to Being
+        Absolutely True Adventures of Captain Pringle and His Crew.’ On the
+        maybe mostly true but somewhat exaggerated shenanigans of my friends and
+        I in high school.
+      </Paragraph>
+      <Paragraph>
+        Then came the WordPress blog. Simply hosted on BlueHost, or HostGator,
+        one of those two. And which got a little more serious. I wrote 20 pages
+        a week on what I thought were brilliant observations that the world
+        needed to know.
+      </Paragraph>
+      <Paragraph>
+        I became obsessed with SEO and getting the perfect design that could
+        keep readers glued to my long diatribes about life. None of it worked.
+        Although I did start to learn more about web development.
+      </Paragraph>
+      <Paragraph>
+        Eventually, after packing down my WordPress install with way too many
+        plugins on a shared server, I got frustrated without how slow its
+        performance was getting. Lighthouse scores were not satisfactory. So I
+        thought I&apos;d build my own CMS. I bought ‘
+        <A
+          isExternal
+          href="https://smile.amazon.com/PHP-MySQL-Dynamic-Web-Sites/dp/0134301846"
+        >
+          PHP and MySQL for Dynamic Web Sites
+        </A>
+        ’ by Larry Ullman and got to work. The book went over a very simple
+        stack. Setting up a SQL database and using PHP to query its data
+        directly into HTML with PHP. Then using a series of includes functions
+        to pull together a header, footer, and some content. From there it was
+        just a matter of formatting the data onto the page. After I built a
+        “backend” that consisted of a few forms to create or update blogs and
+        pages, all I had to do was use FileZilla to FTP the files over to
+        BlueHost, and presto, I had myself a blog.
+      </Paragraph>
+      <Paragraph>
+        That site certainly served its purpose. It wasn&apos;t difficult to keep
+        load speed times to less than 300ms. And I could do all the fancy SEO I
+        wanted without having to install a plugin and have to wait on them for
+        updates. It was nice.
+      </Paragraph>
+      <Paragraph>
+        But fast forward to today, and I want to do more. To have something more
+        declarative. To handle the minor details for me as defined by me.
+      </Paragraph>
+      <Paragraph>
+        Enter Gatsby. A tool I was previously skeptical of. I thought it would
+        be very limited and very opinionated. It&apos;s Next.js or bust. But
+        after getting more into how it works and what it can do, I&apos;m more
+        confident it&apos;s the place my content should call home.
+      </Paragraph>
+      <Paragraph>
+        With the brilliant, performant way it handles images, its hydration
+        capabilities for dynamic content, and its extensibility, it&apos;s an
+        obvious choice for my homepage. Add with Sanity as a backend CMS... I
+        can just define by schema and generate the create post forms? Sold!
+      </Paragraph>
+      <Paragraph>
+        And so this is the stack <A href="/">ethang.dev</A> will live on for the
+        foreseeable future. And where I&apos;ll be writing my thoughts on code
+        and fleshing out my portfolio. Expect to see more to come. I can&apos;t
+        wait to get to work on refining what&apos;s here.
+      </Paragraph>
+    </>
+  );
+}
---
+++ app/routes/_blog.blog.pc-backup-in-two-lines.tsx
@@ -0,0 +1,140 @@
+import { A } from '../components/elements/a';
+import { CodeWrapper } from '../components/elements/code-wrapper';
+import { Paragraph } from '../components/elements/paragraph';
+
+export default function () {
+  return (
+    <>
+      <Paragraph>
+        I backed up my entire PC setup with two lines of PowerShell code that I
+        store on a USB.
+      </Paragraph>
+      <CodeWrapper language="powershell">
+        {[
+          'Start-BitsTransfer -Source https://raw.githubusercontent.com/eglove/PSScripts/master/pcSetup.ps1 -Destination script.ps1',
+          '',
+          'Invoke-Expression‘.\\script.ps1’',
+        ]}
+      </CodeWrapper>
+      <Paragraph>
+        Maybe not exactly two lines of code. Maybe it&apos;s two lines to
+        download the actual script which happens to be around 144 lines. I
+        wouldn&apos;t want to store that on a USB for obvious reasons. If I
+        update it, it&apos;s not up to date on the USB. And I&apos;m definitely
+        not going to bother to update it in two places.
+      </Paragraph>
+      <Paragraph>
+        So what exactly does the{' '}
+        <A
+          isExternal
+          href="https://github.com/eglove/ethang-one/blob/master/apps/computer-cli/src/app/commands/powershell-scripts/pcSetup.ps1"
+        >
+          actual script
+        </A>{' '}
+        do? Well, the first step is the most important step and the one that
+        made me realize I could write this script, to begin with. It installs
+        <A
+          isExternal
+          href="https://github.com/eglove/ethang-one/blob/68a44f7ab7e4abd912a50ad56fee3bfd9d604622/apps/computer-cli/src/app/commands/powershell-scripts/pcSetup.ps1#L46"
+        >
+          Chocolatey Pro
+        </A>{' '}
+        including copying over the license file that&apos;s also kept on the
+        same USB.
+      </Paragraph>
+      <Paragraph>
+        Second, while I&apos;m there, I may as well{' '}
+        <A
+          isExternal
+          href="https://github.com/eglove/ethang-one/blob/68a44f7ab7e4abd912a50ad56fee3bfd9d604622/apps/computer-cli/src/app/commands/powershell-scripts/pcSetup.ps1#L54"
+        >
+          set up WSL 2 and Ubuntu
+        </A>
+        . The tricky part I realized during the process of writing this is that
+        even though chocolatey adds its own environment variables, as well as
+        the variables of the software it will later install, that&apos;s not
+        available in the current session of PowerShell. This also means the
+        choco command isn&apos;t available in that current session.
+      </Paragraph>
+      <Paragraph>
+        So, after downloading and installing WSL I had to open up a new
+        PowerShell window, install it via Chocolatey, then start Ubuntu so that
+        I could set up a new user. Every subsequent choco or GitHub CLI command
+        had to be run in its own PowerShell instance.
+      </Paragraph>
+      <Paragraph>
+        With that done, it&apos;s just a matter of{' '}
+        <A
+          isExternal
+          href="https://github.com/eglove/ethang-one/blob/68a44f7ab7e4abd912a50ad56fee3bfd9d604622/apps/computer-cli/src/app/commands/powershell-scripts/pcSetup.ps1#L69"
+        >
+          installing all the software I have listed in an array
+        </A>{' '}
+        via Chocolatey. As well as any PowerShell modules I use, and yarn global
+        installs.
+      </Paragraph>
+      <Paragraph>
+        And I couldn&apos;t forget to{' '}
+        <A
+          isExternal
+          href="https://github.com/eglove/ethang-one/blob/68a44f7ab7e4abd912a50ad56fee3bfd9d604622/apps/computer-cli/src/app/commands/powershell-scripts/pcSetup.ps1#L85"
+        >
+          download, install, and set an environment variable for all my other
+          scripts
+        </A>
+        . But I couldn&apos;t just download them. I had to git clone them. Via
+        the GitHub client. This means I had to install gh cli and authenticate
+        it before moving on. This sets me up for GitHub commands in the future.
+      </Paragraph>
+      <Paragraph>
+        But why stop there? Cortana is still active. My toolbar isn&apos;t set
+        up as I like it. That can all be done by changing Registry settings.
+        Which can be set via PowerShell. So I{' '}
+        <A
+          isExternal
+          href="https://github.com/eglove/ethang-one/blob/68a44f7ab7e4abd912a50ad56fee3bfd9d604622/apps/computer-cli/src/app/commands/powershell-scripts/pcSetup.ps1#L98"
+        >
+          applied any Windows settings
+        </A>{' '}
+        I wanted to save.
+      </Paragraph>
+      <Paragraph>
+        And while I have a script to update software, I allow the JetBrains
+        toolbox to handle IDE updates and generate shell scripts. So I{' '}
+        <A
+          isExternal
+          href="https://github.com/eglove/ethang-one/blob/68a44f7ab7e4abd912a50ad56fee3bfd9d604622/apps/computer-cli/src/app/commands/powershell-scripts/pcSetup.ps1#L109"
+        >
+          updated those settings
+        </A>{' '}
+        as well as the theme and any other settings I have set on IntelliJ. And
+        settings for terminus, can&apos;t forget to backup my{' '}
+        <A
+          isExternal
+          href="https://github.com/eglove/ethang-one/blob/68a44f7ab7e4abd912a50ad56fee3bfd9d604622/apps/computer-cli/src/app/commands/powershell-scripts/pcSetup.ps1#L115"
+        >
+          terminus settings
+        </A>
+        .
+      </Paragraph>
+      <Paragraph>
+        All wrapping up in a{' '}
+        <A
+          isExternal
+          href="https://github.com/eglove/ethang-one/blob/68a44f7ab7e4abd912a50ad56fee3bfd9d604622/apps/computer-cli/src/app/commands/powershell-scripts/pcSetup.ps1#L120"
+        >
+          clean up process
+        </A>{' '}
+        by doing an initial windows update for the clean install, removing any
+        desktop shortcuts generated by the choco installs, running cleanmgr to
+        allow a safe delete of Windows.old and deleting the downloads for WSL
+        and the initial script off the USB.
+      </Paragraph>
+      <Paragraph>
+        I love this little script. Everything backed up in just the way I want
+        it, ready for a fresh Windows install at any time. I honestly wish I had
+        an excuse to run it more often.
+      </Paragraph>
+    </>
+  );
+}
