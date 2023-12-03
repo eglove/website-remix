d8c3e500cf4845c8c7996109256b10a4bfbca5b1
Ethan Glover <hello@ethang.email>
2023-12-03 13:22:59 -0600

    Update project builder script name
    Changed the script name in 'build.mjs' from "scripts" to "website-remix". This is to depict the accurate script that is being called during the build process, ensuring the correct content management for the website remix project.

--- build.mjs
+++ build.mjs
@@ -1,6 +1,6 @@
 import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
-await projectBuilder("scripts", "master", {
+await projectBuilder("website-remix", "master", {
   preVersionBumpScripts: ["UPDATE", "PRUNE"],
   postVersionBumpScripts: ["DEDUPE", "LINT", "BUILD"],
   isIgnoringBuild: true,
