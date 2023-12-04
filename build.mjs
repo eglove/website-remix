7ec44562d1402dde5acbf5180622e0308a5b8473
Ethan Glover <hello@ethang.email>
2023-12-04 07:52:46 -0600

    update builder

--- build.mjs
+++ build.mjs
@@ -3,6 +3,5 @@ import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 await projectBuilder("website-remix", "master", {
   preVersionBumpScripts: ["UPDATE", "PRUNE"],
   postVersionBumpScripts: ["DEDUPE", "LINT", "BUILD"],
-  isIgnoringBuild: true,
-  isIgnoringPeerDependencies: true,
+  isLibrary: false,
 });
